2d2ef373e4f768f6108c7c7ca7e583e0ae24c38a
Rafał Bastek <rafun2014@gmail.com>
2025-04-07 09:46:07 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#Hi I hope we will get along together!
+
+to_do_list =["pójść do chirurga","pójść do stomatologa","pójść do lekarza onkologicznego","pójść do endokrynologa","zapytać się w pracy co z piątkiem 9:30"]
+for each in to_do_list:
+    print(each)
