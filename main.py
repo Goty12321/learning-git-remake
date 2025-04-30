c9ea259f3d6dd8387c4cb977870cb2ce45c8b118
Goty12321 <112967365+Goty12321@users.noreply.github.com>
2025-04-30 16:58:20 +0200

    Adding values to list main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Hi I hope we will get along together!
 
-to_do_list =["pójść do parku","pójść do sklepu","pójść do znajomego","pójść na pizze"]
+to_do_list =["pójść do parku","pójść do sklepu","pójść do znajomego","pójść na pizze","go cry"]
 for each in to_do_list:
     print(each)
