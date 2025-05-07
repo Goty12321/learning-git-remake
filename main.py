c4780deef5dedfffa632a974e098e40ff3a6ffa7
Goty12321 <112967365+Goty12321@users.noreply.github.com>
2025-05-07 11:27:40 +0200

    Add walking to main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Hi I hope we will get along together!
 
-to_do_list =["pójść do sklepu","pójść do znajomego","pójść na pizze","go cry"]
+to_do_list =["pójść do sklepu","pójść do znajomego","pójść na pizze","go for a walk"]
 for each in to_do_list:
     print(each)
