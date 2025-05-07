61001ed03f127952371b18b962fc7d85d96e2d49
Goty12321 <112967365+Goty12321@users.noreply.github.com>
2025-05-07 11:30:34 +0200

    Add number main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Hi I hope we will get along together!
-
+number = 14
 to_do_list =["pójść do sklepu","pójść do znajomego","pójść na pizze","go for a walk"]
 for each in to_do_list:
     print(each)
