696d1edb3193c653d35c1af446c20077443b6ff8
Rafał Bastek <rafun2014@gmail.com>
2025-05-08 09:28:54 +0200

    Add main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-#Hey, trying to do it all again!
-for numbers in range (1,11):
-    print(numbers)
-    
+lista_zakupów = {
+    "piekarnia": ['chleb','pączek','bułki','gniazdko'],
+    "warzywniak": ['marchew','seler','rukola'],
+    "stacja benzynowa": ['hotdog]']
+}
+ilość_produktów = 0
+for key in lista_zakupów:
+    values = lista_zakupów[key]
+    ilość_produktów = ilość_produktów + len(values)
+    updated_values = []
+    for value in values:
+        updated_values.append(value.capitalize())
+    print(f"Idę do {key.capitalize()} i kupuję tam {updated_values}.")
+print(f"W sumie kupuję {ilość_produktów} produktów.")
